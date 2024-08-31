cbaa2375439d097dc98f6ee7731883b03b0777b9
palaksingla2004 <palaksingla1401@gmail.com>
2024-08-31 20:39:40 +0530

    git

---
+++ 2908/code2.js
@@ -0,0 +1,42 @@
+const students = [
+    {
+    name: "Palak",
+    age: 20,
+    marks:45,
+    subject: "English"
+    },
+    {
+    name: "Ojasva",
+    age: 22,
+    marks: 50,
+    subject: "Math"
+    },
+    {
+      name: "Pranya",
+      age: 21,
+      marks: 48,
+      subject: "Science"
+    },
+    {
+      name: "Prerna",
+      age: 20,
+      marks: 42,
+      subject: "History" 
+    },
+    {
+      name: "Sarah",
+    age: 23,
+    marks: 56,
+    subject: "Computer Science"},
+    {
+      name: "David",
+      age: 24,
+      marks: 52,
+      subject: "Maths"
+      },
+    {name: "Ram",
+      age: 25,
+      marks: 58,
+      subject: "English"}
+    
+  ]
---
+++ 2908/readme.md
@@ -0,0 +1,6 @@
+QUESTIONS:
+1. Find the data where student has age = 22 and marks = 90
+
+2. Find the student whose marks is >= 80 and age >= 18
+
+3. Find whose grades are A++
